90ae73586aecd5975d7b1664e093fa6803d70fb0
Rahul Singh <singh.rahul26@hotmail.com>
2018-12-19 13:40:42 +0530

    added start, stop network script

---
+++ network/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+# stop the network
+docker-compose -f docker-compose-cli.yaml down
+
+# start the network
+docker-compose -f docker-compose-cli.yaml up -d
+if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to start network"
+    exit 1
+fi
+
+# now run the end to end script
+docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Test failed"
+    exit 1
+fi
