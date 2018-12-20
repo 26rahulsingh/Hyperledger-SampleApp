918d1d21552a086c10bea21aff45148a8dab2afb
Rahul Singh <singh.rahul26@hotmail.com>
2018-12-20 14:48:58 +0530

    Failed to reach implicit threshold of 1 sub-policies, required 1 remaining

--- network/teardown.sh
+++ network/teardown.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Shut down the Docker containers for the system tests.
-# docker-compose -f docker-compose-cli.yaml kill && docker-compose -f docker-compose-cli.yaml down
-
-# remove the local state
-# rm -f ~/.hfc-key-store/*
-
-# remove chaincode docker images
-# docker rm $(docker ps -aq)
-# docker rmi $(docker images dev-* -q)
-
-# Your system is now clean
-
-
 # stop all containers.
 docker-compose -f docker-compose-cli.yaml down --volumes --remove-orphans
+
+# Shut down the Docker containers for the system tests.
+docker-compose -f docker-compose-cli.yaml kill && docker-compose -f docker-compose-cli.yaml down
+
+# remove the local state
+rm -f ~/.hfc-key-store/*
+
+# remove chaincode docker images
+docker rm $(docker ps -aq)
+docker rmi $(docker images dev-* -q)
+
+# Your system is now clean
