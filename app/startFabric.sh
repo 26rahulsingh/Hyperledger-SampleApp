6d71b5a40a60dab86d370791dcd9a9a18a32de71
Rahul Singh <singh.rahul26@hotmail.com>
2018-12-24 19:18:22 +0530

    changes in startFabric.sh file

--- app/startFabric.sh
+++ app/startFabric.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#
-# SPDX-License-Identifier: Apache-2.0
-# This code is based on code written by the Hyperledger Fabric community. 
-# Original code can be found here: https://github.com/hyperledger/fabric-samples/blob/release/fabcar/startFabric.sh
-#
 # Exit on first error
 
 set -e
@@ -18,17 +13,17 @@ if [ ! -d ~/.hfc-key-store/ ]; then
 fi
 
 # launch network; create channel and join peer to channel
-cd ../basic-network
+cd ../network
 ./start.sh
 
 # Now launch the CLI container in order to install, instantiate chaincode
 # and prime the ledger with our 10 tuna catches
-docker-compose -f ./docker-compose.yml up -d cli
+docker-compose -f ./docker-compose-cli.yaml up -d cli
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+docker exec -e "CORE_PEER_LOCALMSPID=100MBMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/100mb.jet-network.com/users/Admin@100mb.jet-network.com/msp" cli peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=100MBMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/100mb.jet-network.com/users/Admin@100mb.jet-network.com/msp" cli peer chaincode instantiate -o orderer.jet-network.com:7050 -C mychannel -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('100MBMSP.peer','ThinkRightMSP.peer')"
 sleep 10
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+docker exec -e "CORE_PEER_LOCALMSPID=100MBMSP" -e "CORE_PEER_MSPCONFIGPATH=opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/100mb.jet-network.com/users/Admin@100mb.jet-network.com/msp" cli peer chaincode invoke -o orderer.jet-network.com:7050 -C mychannel -n tuna-app -c '{"function":"initLedger","Args":[""]}'
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
 printf "\nStart with the registerAdmin.js, then registerUser.js, then server.js\n\n"
